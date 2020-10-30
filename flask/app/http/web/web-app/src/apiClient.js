fa6ff4f00f0608995cc5f47d7412d9692d49d992
dhruvi16 <dhruvibutti9477@gmail.com>
2020-10-30 22:02:05 +0530

    Add API Client to the app

---
+++ flask/app/http/web/web-app/src/apiClient.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const BASE_URI = 'http://localhost:4433';
+
+const client = axios.create({
+ baseURL: BASE_URI,
+ json: true
+});
+
+class APIClient {
+ constructor(accessToken) {
+   this.accessToken = accessToken;
+ }
+
+ createTask(task) {
+   return this.perform('post', '/tasks', task);
+ }
+
+ deleteTask(task) {
+   return this.perform('delete', `/task/${task.id}`);
+ }
+
+ getTasks() {
+   return this.perform('get', '/tasks');
+ }
+
+ async perform (method, resource, data) {
+   return client({
+     method,
+     url: resource,
+     data,
+     headers: {
+       Authorization: `Bearer ${this.accessToken}`
+     }
+   }).then(resp => {
+     return resp.data ? resp.data : [];
+   })
+ }
+}
+
+export default APIClient;
